8023fbcfc302a7297fc3a9766aeb82dd4ebb3e29
Carlos Eduardo <eduardosantn060705@gmail.com>
2025-03-26 19:59:11 -0300

    Implementando Login. Conectando com o Banco de Dados e com o front-end

---
+++ appLogin/src/main/java/br/appLogin/appLogin/LoginController.java
@@ -0,0 +1,14 @@
+package br.appLogin.appLogin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "index";
+	}
+	
+}
